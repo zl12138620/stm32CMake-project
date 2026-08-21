bcde30ebd78ddd935f92e7dd23c21b7fab4f24ef
铭曰悟饭 <3097632322@qq.com>
2026-08-21 19:53:49 +0800

    Initial commit: STM32F103 CMake project

---
+++ toolchain.cmake
@@ -0,0 +1,26 @@
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY) #编译目标设置为静态
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+
+option(USE_ARMGCC "USE ARMGCC" ON)
+
+if(USE_ARMGCC)
+{
+    #指定编译器目录
+    set(COMPILE_ROOT_PATH E:/Project/Compiler/arm-gnu-toolchain-15.3.rel1-mingw-w64-i686-arm-none-eabi/bin)
+    #头文件目录
+    set(CMAKE_C_COMPILER ${COMPILE_ROOT_PATH}/arm-none-eabi-gcc.exe)
+    set(CMAKE_ARM_COMPILER ${COMPILE_ROOT_PATH}/arm-none-eabi-gcc.exe)
+    set(CMAKE_OBJCOPY ${COMPILE_ROOT_PATH}/arm-none-eabi-objcopy.exe)
+    set(CMAKE_SIZE ${COMPILE_ROOT_PATH}/arm-none-eabi-size.exe)
+}
+
+
+
+
+
+
