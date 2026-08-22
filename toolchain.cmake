efcc416a07f34673e9d177ce7676d1db6bcafbba
铭曰悟饭 <3097632322@qq.com>
2026-08-22 18:14:44 +0800

    Fix GNU toolchain build: use GAS startup file, add main.c, fix linker script and CMake options

--- toolchain.cmake
+++ toolchain.cmake
@@ -9,15 +9,15 @@ set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 option(USE_ARMGCC "USE ARMGCC" ON)
 
 if(USE_ARMGCC)
-{
     #指定编译器目录
     set(COMPILE_ROOT_PATH E:/Project/Compiler/arm-gnu-toolchain-15.3.rel1-mingw-w64-i686-arm-none-eabi/bin)
     
     set(CMAKE_C_COMPILER ${COMPILE_ROOT_PATH}/arm-none-eabi-gcc.exe)
+    set(CMAKE_ASM_COMPILER ${COMPILE_ROOT_PATH}/arm-none-eabi-gcc.exe)
     set(CMAKE_ARM_COMPILER ${COMPILE_ROOT_PATH}/arm-none-eabi-gcc.exe)
     set(CMAKE_OBJCOPY ${COMPILE_ROOT_PATH}/arm-none-eabi-objcopy.exe)
     set(CMAKE_SIZE ${COMPILE_ROOT_PATH}/arm-none-eabi-size.exe)
-}
+endif()
 
 
 
